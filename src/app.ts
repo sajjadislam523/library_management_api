d582949f5a9654afbb69240ee269fcd46ae0a806
sajjadislam523 <sajjad.islam523@gmail.com>
2026-03-08 01:29:37 +0600

    feat: add database connection and Vercel configuration

--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,28 @@
 import express, { type Application } from "express";
+import { connectDB } from "./app/config/db.js";
+import { globalErrorHandler } from "./app/middlewares/globalErrorHandler.js";
 import bookRouter from "./app/modules/book/book.routes.js";
 import borrowRouter from "./app/modules/borrow/borrow.routes.js";
 
 export const app: Application = express();
 app.use(express.json());
 
-app.use("/api/books", bookRouter);
-app.use("/api/borrow", borrowRouter);
+app.use(async (req, res, next) => {
+    try {
+        await connectDB();
+        next();
+    } catch (error) {
+        next(error);
+    }
+});
 
 app.get("/", (req, res) => {
     res.send("Welcome to the library!");
 });
+
+app.use("/api/books", bookRouter);
+app.use("/api/borrow", borrowRouter);
+
+app.use(globalErrorHandler);
 
 export default app;
