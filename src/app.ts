57de184ec1c76123accb9631c4d2809fe4ad8fca
sajjadislam523 <sajjad.islam523@gmail.com>
2026-03-08 01:33:38 +0600

    refactor: remove database connection logic and related file

--- src/app.ts
+++ src/app.ts
@@ -1,20 +1,10 @@
 import express, { type Application } from "express";
-import { connectDB } from "./app/config/db.js";
 import { globalErrorHandler } from "./app/middlewares/globalErrorHandler.js";
 import bookRouter from "./app/modules/book/book.routes.js";
 import borrowRouter from "./app/modules/borrow/borrow.routes.js";
 
 export const app: Application = express();
 app.use(express.json());
-
-app.use(async (req, res, next) => {
-    try {
-        await connectDB();
-        next();
-    } catch (error) {
-        next(error);
-    }
-});
 
 app.get("/", (req, res) => {
     res.send("Welcome to the library!");
